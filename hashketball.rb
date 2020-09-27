7e6e55da072b4809ba92ac1dc17b5b55b4934fdd
Sharad Satsangi <s.satsangi@gmail.com>
2020-09-27 07:45:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,10 +2,14 @@
 def game_hash
   return {
     :home => {
-      
+      :team_name => "",
+      :colors => [],
+      :players => []
     },
     :away => {
-      
+      :team_name => "",
+      :colors => [],
+      :players => []
     }
   }
 end
